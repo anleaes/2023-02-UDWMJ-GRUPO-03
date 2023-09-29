61bc9c4b73b6a7b9a370c6409cf544d0c0620019
Guilherme-C-Ferrari <guicferrari@hotmail.com>
2023-09-29 09:01:19 -0300

    atualização da classe product

--- product.py
+++ product.py
@@ -1,5 +1,8 @@
 class Product:
 
-    def __init__(self, name, category):
+    def __init__(self, name, description, date_fabrication, is_active, category):
         self._name = name
+        self._description = description
+        self._date_fabrication = date_fabrication
+        self._is_active = is_active
         self._category = category
