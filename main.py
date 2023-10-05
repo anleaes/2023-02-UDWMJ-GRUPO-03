98542bbd6fca43321a1e63141008c8ff8f9cfca3
joao07neumann <joaorqn@gmail.com>
2023-10-05 11:33:01 -0300

    Implementação classe socialnetwork na main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from category import Category
 from client import Client
+from social_network import SocialNetwork
 
 def main():
     categoria = Category(1234, "Ferramentas", "Categoria que representa ferramentas")
@@ -7,6 +8,9 @@ def main():
     
     client_obj = Client("Gustavo", "Dias", "Rua A, 22", 51935728356, "gustavo.dias@gmail.com", "M")
     client_obj.change_cellphone(51910001000)
+
+    teiasocial = SocialNetwork("fbook", "fácil de usar",)
+    teiasocial.show_description()
 
 if __name__ == "__main__":
     main()
