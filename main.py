e04b4bb6165afeb7ebeb0e0e0b6b52de151aaf7d
g-lopesalves <lopesgabriel63@gmail.com>
2023-10-05 11:17:17 -0300

    implementando client em main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from client import Client
+
 def main():
-    pass
+    client_obj = Client("Gustavo", "Dias", "Rua A, 22", 51935728356, "gustavo.dias@gmail.com", "M")
+    client_obj.change_cellphone(51910001000)
 
 
 if __name__ == "__main__":
