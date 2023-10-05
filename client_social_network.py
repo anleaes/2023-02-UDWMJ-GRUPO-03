de2c185ae19a3c5ba57150a68bb58ae3bc23ceea
g-lopesalves <lopesgabriel63@gmail.com>
2023-10-05 10:59:53 -0300

    criação método em client_social_network

--- client_social_network.py
+++ client_social_network.py
@@ -4,3 +4,7 @@ class ClientSocialNetwork:
         self._client = client
         self._social_network = social_network
     
+
+    def show_client_name_and_social_network(self):
+        print(f"Client name: {self._client._first_name} {self._client._last_name}")
+        print(f"Social Network: {self._social_network._name}")
