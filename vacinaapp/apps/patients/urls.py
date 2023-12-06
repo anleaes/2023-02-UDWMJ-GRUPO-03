a07997d1cf04cb1cb17754ed0b211d03e5bc0a0a
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2023-12-06 19:34:11 -0300

    Implementação URLs

---
+++ vacinaapp/apps/patients/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'patient'
+
+urlpatterns = [
+    path('', views.list_patient, name='list_patient'),
+    path('adicionar/', views.add_patient, name='add_patient'),
+    path('editar/<int:id_patient>/', views.edit_patient, name='edit_patient'),
+    path('excluir/<int:id_patient>/', views.delete_patient, name='delete_patient'),
+]
