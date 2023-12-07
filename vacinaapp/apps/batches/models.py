94826614ea81ccb9456da2f90559e7ae3b013748
joao <joaorqn@gmail.com>
2023-12-07 20:42:33 -0300

    Implementação Correções

--- vacinaapp/apps/batches/models.py
+++ vacinaapp/apps/batches/models.py
@@ -6,7 +6,7 @@ from brands.models import Brand
 class Batch(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    number = models.IntegerField('Numero do Lote')
+    number = models.CharField('Numero do Lote', max_length=7)
     date = models.DateField('Data de Fabricacao', auto_now=False, auto_now_add=False)
     expiration_date = models.DateField('Data Expiracao', auto_now=False, auto_now_add=False)
     quantity = models.IntegerField('Quantidade de Vacinas')
